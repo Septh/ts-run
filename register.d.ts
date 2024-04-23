dc6e10d5df05b449f1d85dd63b354c7658871cd2
Stephan Schreiber <septh@users.noreply.github.com>
2024-04-23 10:20:11 +0200

    style: Add JSDoc comment in register.d.ts

--- register.d.ts
+++ register.d.ts
@@ -1,1 +1,7 @@
+/**
+ * This module does not export anything. It can only be imported for its side-effect
+ * (which is registering the custom loaders).
+ *
+ * `import "@septh/ts-run"`
+ */
 export {}
