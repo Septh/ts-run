1b6aa8f794cb9d4fe4460b93ad32e18eebb9cc68
Stephan Schreiber <septh@users.noreply.github.com>
2025-05-10 18:23:44 +0200

    feat: better unknown argument handling

--- source/bin/index.ts
+++ source/bin/index.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { resolve } from 'node:path'
+import { resolve, sep } from 'node:path'
 import { pathToFileURL } from 'node:url'
 import { createRequire } from 'node:module'
 
@@ -14,7 +14,18 @@ if (argv.length >= 3 && !argv[2].startsWith('-')) {
     await import(pathToFileURL(argv[1]).href)
 }
 else {
-    // Print usage.
-    const { name } = createRequire(import.meta.url)('#package.json') as typeof import('#package.json')
-    console.log(`Usage: ${name} path/to/script.ts`)
+    // Respond to `ts-run -v | --version` or print usage.
+    const { name, version } = createRequire(import.meta.url)('#package.json') as typeof import('#package.json')
+    const shortName = name.split('/').pop()
+
+    if (argv.length > 2) {
+        if (/^-v|--version$/.test(argv[2])) {
+            console.log(`Node.js v${process.versions.node}, ${shortName} v${version}`)
+        }
+        else if (argv[2].startsWith('-')) {
+            console.error(`${shortName}: unknown argument ${argv[2]}. Script arguments must be written *after* the name of the script.`)
+            process.exitCode = 1
+        }
+    }
+    else console.log(`Usage: ${shortName} -v | path${sep}to${sep}script.ts`)
 }
