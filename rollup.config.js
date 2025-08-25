ed0ea67e0ee8b777dbfa6659c23d284fd4cb893c
Stephan Schreiber <septh@users.noreply.github.com>
2025-08-25 09:18:39 +0200

    build: group imports

--- rollup.config.js
+++ rollup.config.js
@@ -3,19 +3,22 @@ import { readFile } from 'node:fs/promises'
 import { defineConfig } from 'rollup'
 import { nodeExternals } from 'rollup-plugin-node-externals'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonJS_ from '@rollup/plugin-commonjs'
 import { sucrase } from 'rollup-plugin-fast-typescript'
+import terser_ from '@rollup/plugin-terser'
 import { codeRaker } from 'rollup-plugin-code-raker'
 
 /**
  * Workaround for the wrong typings in all rollup plugins
  * (see https://github.com/rollup/plugins/issues/1541#issuecomment-1837153165)
+ *
  * @template T
- * @param {{ default: { default: T } }} plugin
+ * @param { {default: T} } plugin
  * @returns {T}
  */
-const rollupPlugin = ({ default: plugin }) => /** @type {T} */(plugin)
-const commonJS = rollupPlugin(await import('@rollup/plugin-commonjs'))
-const terser = rollupPlugin(await import('@rollup/plugin-terser'))
+const fixRollupPlugin = (plugin) => /** @type {T} */(plugin)
+const commonJS = fixRollupPlugin(commonJS_)
+const terser = fixRollupPlugin(terser_)
 
 export default defineConfig([
 
