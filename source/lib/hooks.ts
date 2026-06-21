7a7110e53aae6a9c9997908a8593b7f2787fc5ee
Stephan Schreiber <septh@users.noreply.github.com>
2026-06-21 14:30:22 +0200

    fix: throw on errors other than ENOENT in `nearestPackageType()`

--- source/lib/hooks.ts
+++ source/lib/hooks.ts
@@ -24,22 +24,22 @@ if (major < 23 || (major === 23 && minor < 4)) {
 
 function nearestPackageType(file: string, defaultType: NodeJS.ModuleType): NodeJS.ModuleType {
     for (let current = path.dirname(file), previous = ''; previous !== current; previous = current, current = path.dirname(current)) {
-        const pkgFile = path.join(current, 'package.json')
-        let cached = pkgTypeCache.get(pkgFile)
+        const pathKey = path.join(current, 'package.json')
+        let cached = pkgTypeCache.get(pathKey)
         if (cached === undefined) {
             try {
-                const buffer = readFileSync(pkgFile)
+                const buffer = readFileSync(pathKey)
                 const { type } = JSON.parse(buffer.toString()) as PackageJson
                 cached = (type === 'module' || type ==='commonjs') ? type : defaultType
             }
             catch (err) {
-                const { code, message } = err as NodeJS.ErrnoException
+                const { code } = err as NodeJS.ErrnoException
                 if (code !== 'ENOENT')
-                    console.error(styleText('red', message))
+                    throw err
                 cached = null
             }
 
-            pkgTypeCache.set(pkgFile, cached)
+            pkgTypeCache.set(pathKey, cached)
         }
 
         if (typeof cached === 'string')
